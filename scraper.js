6f31321bab2a0086da483bbfda2a9d9e628eefe2
wowiamhere <wowiamhere@gmail.com>
2022-01-05 02:20:14 -0800

    gets all posts ids

--- scraper.js
+++ scraper.js
@@ -1,3 +1,26 @@
 // for puppeteer
 const pup = require('puppeteer')
 
+async function getData(){
+
+	const browser = await pup.launch()
+	const page = await browser.newPage()
+
+	await page.goto('https://www.facebook.com/pg/centralcastinglosangeles/posts/', 
+		{ waitUntil: 'load' }).then( (response) => { console.log(response.status())} )
+
+	await page.screenshot({path: 'tst.png', fullPage: true})
+
+	const info = await page.$$eval('a[class=see_more_link]', 
+		(spans) => spans.map( (span) => 
+			span.href).map( (post_id) => 
+				post_id.match(/\d+$/)
+				)
+			)
+
+console.log("------->" + info)	
+
+	await browser.close()
+}
+
+getData()
