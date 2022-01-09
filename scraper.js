c7081fc13bf0e9d6976846ee2c10e01b6e45b517
wowiamhere <wowiamhere@gmail.com>
2022-01-08 18:39:24 -0800

    added views

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 // for puppeteer
 const pup = require('puppeteer')
 
-async function getData(){
+async function getData(req, res, next){
 
 	const browser = await pup.launch()
 	const page = await browser.newPage()
@@ -9,18 +9,20 @@ async function getData(){
 	await page.goto('https://www.facebook.com/pg/centralcastinglosangeles/posts/', 
 		{ waitUntil: 'load' }).then( (response) => { console.log(response.status())} )
 
-	await page.screenshot({path: 'tst.png', fullPage: true})
+//	await page.screenshot({path: 'tst.png', fullPage: true})
 
-	const info = await page.$$eval('a[class=see_more_link]', 
+	res.info = await page.$$eval('span[class=text_exposed_link]>a',
 		(spans) => spans.map( (span) => 
 			span.href).map( (post_id) => 
-				post_id.match(/\d+$/)
+				post_id.match( /(?<=posts\/)\d+/ )
 				)
 			)
 
-console.log("------->" + info)	
+console.log("------->" + res.info)	
 
 	await browser.close()
+	next()
+
 }
 
-getData()
+module.exports = getData
